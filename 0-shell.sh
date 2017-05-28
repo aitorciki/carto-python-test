f908ccc94011f661905e7745eea81609398ae892
Aitor Guevara <aitor@newrelic.com>
2017-05-28 18:38:10 +0200

    shell: allow passing URL and column name to average as params

--- 0-shell.sh
+++ 0-shell.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+URL=${1:-https://s3.amazonaws.com/carto-1000x/data/yellow_tripdata_2016-01.csv}
+COLUMN_NAME=${2:-tip_amount}
+
 TMPFILE=$(mktemp)
 
-wget -q -O $TMPFILE https://s3.amazonaws.com/carto-1000x/data/yellow_tripdata_2016-01.csv
-FIELD=$(head -n1 $TMPFILE | tr "," "\n" | grep -nx tip_amount |  cut -d":" -f1)
+wget -q -O $TMPFILE "$URL"
+FIELD=$(head -n1 $TMPFILE | tr "," "\n" | grep -nx "$COLUMN_NAME" |  cut -d":" -f1)
 awk -F , -v field=$FIELD '
     {
         sum += $field;
